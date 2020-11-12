e37f575e8d4b0920f40eb87ea31d9be0656b9cbe
Zhenhua yu <zyu0628@gmail.com>
2020-11-12 18:50:11 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -18,3 +18,8 @@ function pressIt(){
     }
   })
 }
+function submitIt(){
+  $("form").on("submit", function() {
+  if ($( "input:first" ).val() === "correct") {
+    alert('your form is going to be submitted now');
+}
