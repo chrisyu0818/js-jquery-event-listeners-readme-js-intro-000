436d96e7c82dfd67bf6a81dfc678b78f832b1192
Zhenhua yu <zyu0628@gmail.com>
2020-11-12 18:51:03 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -20,6 +20,5 @@ function pressIt(){
 }
 function submitIt(){
   $("form").on("submit", function() {
-  if ($( "input:first" ).val() === "correct") {
-    alert('your form is going to be submitted now');
+    alert('Your form is going to be submitted now.')
 }
